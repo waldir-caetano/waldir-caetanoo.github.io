ebc1a93ea38af52b26ed5dec83bb4b925bd698de
waldir-caetano <167473975+waldir-caetano@users.noreply.github.com>
2024-04-20 23:42:21 +0100

    portfólio

---
+++ script.js
@@ -0,0 +1,90 @@
+function mostrarcs(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-cs");
+
+    divGeral.style.display="block";
+    div.style.display="block";
+}
+function ocultarcs(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-cs");
+
+    divGeral.style.display="none";
+    div.style.display="none";
+
+}
+function mostrarhtml(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-html");
+
+    divGeral.style.display="block";
+    div.style.display="block";
+}
+function ocultarhtml(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-html");
+
+    divGeral.style.display="none";
+    div.style.display="none";
+
+}
+function mostrarcss(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-css");
+
+    divGeral.style.display="block";
+    div.style.display="block";
+}
+function ocultarcss(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-css");
+
+    divGeral.style.display="none";
+    div.style.display="none";
+
+}
+function mostrarjs(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-js");
+
+    divGeral.style.display="block";
+    div.style.display="block";
+}
+function ocultarjs(){
+    var divGeral=document.querySelector("#principal");
+    var div=document.querySelector("#desc-js");
+
+    divGeral.style.display="none";
+    div.style.display="none";
+
+}
+function mostrarwl(){
+    var divGeral=document.querySelector("#principall");
+    var div=document.querySelector("#desc-wl");
+
+    divGeral.style.display="block";
+    div.style.display="block";
+}
+function ocultarwl(){
+    var divGeral=document.querySelector("#principall");
+    var div=document.querySelector("#desc-wl");
+
+    divGeral.style.display="none";
+    div.style.display="none";
+
+}
+function mostrarpr(){
+    var divGeral=document.querySelector("#principall");
+    var div=document.querySelector("#desc-pr");
+
+    divGeral.style.display="block";
+    div.style.display="block";
+}
+function ocultarpr(){
+    var divGeral=document.querySelector("#principall");
+    var div=document.querySelector("#desc-pr");
+
+    divGeral.style.display="none";
+    div.style.display="none";
+
+}
